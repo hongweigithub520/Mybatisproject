21bb2b8e30aacce9ef63527e4b547a33c0aec7fd
hongweiyy <2272715149@qq.com>
2020-08-04 22:43:26 +0800

    首次创建本地仓库

---
+++ src/main/java/com/hw/mybatis/helloworld.java
@@ -0,0 +1,23 @@
+package com.hw.mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class helloworld {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("hello");
+    }
+    @Test
+    public void test() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+   }
